fcfc21ab1b48b41d35db3cfb1501cc22c2de8e73
krisp89 <56803934+krisp89@users.noreply.github.com>
2025-02-26 18:45:49 +0200

    Add files via upload
    Stuck on the last step on how to print out the student with the lowest score and the one with the highest score (line 19 and 20)

---
+++ task_min_max_student_score.py
@@ -0,0 +1,20 @@
+students_score = {
+    'Ivan': 5.00,
+    'Alex': 3.50,
+    'Maria': 5.50,
+    'Georgy':5.00
+}
+
+# Print out the name and score of the student with maximum score
+# Print out the name and score of the student with minimum score
+# Maria - 5.5
+# Alex - 3.5
+
+# To obtain the max_value in the dict
+max_value= max(students_score.values())
+# To obtain the min value in the dict
+min_value= min(students_score.values())
+
+
+# for name,score in students_score.items():
+#     print(f'name' {max_value})
